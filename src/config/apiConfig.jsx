7f528caf9aaf802b0f3a378b7933c98242866913
Joan-Acevedo <joan.acevedo@mail.escuelaing.edu.co>
2025-05-18 19:19:57 -0500

    Cambio de nombre variable de entorno

--- src/config/apiConfig.jsx
+++ src/config/apiConfig.jsx
@@ -3,7 +3,7 @@
  * Utiliza variables de entorno con valores por defecto como respaldo
  */
 const API_CONFIG = {
-  BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080',
+  BASE_URL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080',
 };
 
 export default API_CONFIG;
