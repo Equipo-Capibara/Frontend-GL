ac46652227c45a77eceee00a937dedcf03205363
Joan-Acevedo <acevedojoan22@gmail.com>
2025-05-21 18:54:06 -0500

    Ganar y llaves distintas

--- src/components/game/Key.jsx
+++ src/components/game/Key.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import '../../styles/tile.css';
 
-const Key = () => {
-  return <img src={`/resources/key.png`} alt="Key" className="tile-content-key" />;
+const Key = ({ type }) => {
+  return <img src={`/resources/${type.toLowerCase()}.png`} alt={type} className="tile-content-key" />;
 };
 
 export default Key;
